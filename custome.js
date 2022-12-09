3ea07c26b1ce63f76312d875171fb74a20eca8c8
satyapanchal76 <119926162+satyapanchal76@users.noreply.github.com>
2022-12-09 15:43:59 +0530

    Add files via upload

---
+++ custome.js
@@ -0,0 +1,9 @@
+
+ // // Preloader
+ //  $(window).on('load', function() {
+ //    if ($('#preloader').length) {
+ //      $('#preloader').delay(1500).fadeOut('slow', function() {
+ //        $(this).remove();
+ //      });
+ //    }
+ //  });
